d6a8a8cbcfd0c37928952900a962981c2201db7f
Zarko Hristovski <zarko@timegrip.no>
2016-03-11 15:47:05 +0100

    A little bit of polish

--- bin/spfile.js
+++ bin/spfile.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --harmony
 
 'use strict';
 
@@ -6,93 +6,76 @@ const
   os   = require( 'os'   ),
   fs   = require( 'fs'   ),
   url  = require( 'url'  ),
-  path = require( 'path' ),
+  path = require( 'path' );
 
-  get_cookie_path = ( opts ) => {
-    opts = opts || {};
-    let cookie_dir = path.join(
-      process.env.HOME || process.env.USERPROFILE || process.env.HOMEPATH,
-      '.sharepoint-file'
-    );
-    ! fs.existsSync( cookie_dir ) && fs.mkdirSync( cookie_dir );
-    return opts.only_dir ? cookie_dir : path.join( cookie_dir, 'cookies.json' );
-  };
-
-let command = process.argv[ 2 ];
-command = command ? command.trim().toLowerCase() : '';
-if ( command.length && [
-  'cleanup', 'login', 'logout', 'fetch', '-h', '--help', '-v', '--ver', '--version'
-].indexOf( command ) === -1 ) {
-  console.log( 'Command not found.' );
-  process.exit( 1 );
-}
-
-if ( [ '-v', '--ver', '--version' ].indexOf( command ) !== -1 ) {
-  console.log(`
-    sharepoint-file ver. ${require( '../package.json' ).version }
-  `);
-  return;
-}
-
-if ( ! command.length || [ '-h', '--help' ].indexOf( command ) !== -1 ) {
-  console.log(`
-    sharepoint-file ver. ${require( '../package.json' ).version }
-
-    - Fetch a file (shows its content):
-
-      $ spfile fetch <sharepoint file url>
-
-      Example: spfile fetch https://your.sharepoint.com/path/foo.json
-
-    - Fetch a file and save it:
-
-      $ spfile fetch <sharepoint file url> <file destination path>
-
-      Example: spfile fetch https://your.sharepoint.com/path/bar.pdf bar.pdf
-
-      If you haven't already authenticated or your session has expired,
-      you'll be asked to log in by typing in your Sharepoint/Office 365
-      email address and password.
-
-    - For explicit logout or login use the corresponding command:
-
-      $ spfile logout
-
-      $ spfile login <sharepoint host url>
-
-      Example: spfile login https://your.sharepoint.com
-
-    - Show usage instruction:
-
-      $ spfile (--help)
-  `);
-  return;
-}
-
-if ( command === 'cleanup' ) {
+if ( process.argv.slice( 2 ).map( arg => arg.toLowerCase() ).indexOf( 'logout' ) !== -1 ) {
   require( 'child_process' ).execSync(
-    `${process.platform.startsWith( 'win' ) ? 'rmdir /s/q' : 'rm -rf' } ${get_cookie_path({ only_dir : true })}`
+    `${process.platform.startsWith( 'win' ) ? 'rmdir /s/q' : 'rm -rf' } ${get_cookie_path({ dir : true })}`
   );
-  return;
-}
-
-if ( command === 'logout' ) {
-  try {
-    fs.unlinkSync( get_cookie_path() );
-    console.log( 'Logged out.' );
-  }
-  catch ( e ) {}
+  console.log( 'Logged out.' );
   return;
 }
 
 const
   read            = require( 'read'                   ),
+  colors          = require( 'colors'                 ),
   parser          = require( 'xml2js'                 ),
   concat          = require( 'concat-stream'          ),
+  parseArgs       = require( 'minimist'               ),
   request         = require( 'request'                ),
   Sharepoint      = require( 'sharepoint-auth'        ),
   istextorbinary  = require( 'istextorbinary'         ),
   FileCookieStore = require( 'tough-cookie-filestore' );
+
+const cmd = parse( process.argv.slice( 2 ) );
+if ( ! cmd ) {
+  return;
+}
+
+if ( cmd.name === 'login' ) {
+  login( extract( cmd.args, 'host_url' ) )
+    .then( () => console.log( 'Logged in.' ) )
+    .catch( e => console.log( e ) );
+  return;
+}
+
+if ( cmd.name === 'fetch' ) {
+  login( extract( cmd.args, 'host_url' ) ).then( jar => {
+    cmd.args[ 1 ] && ! cmd.silent && console.log( `Fetching file...` );
+    request({
+      url : `${extract( cmd.args, 'host_url' )}/_api/web/GetFileByServerRelativeUrl('${url.parse( cmd.args[ 0 ] ).path}')/$value`,
+      jar : jar
+    }).on( 'error', err => console.log( err ) ).on( 'response', response => {
+      if ( response.statusCode !== 200 ) {
+        parse_request_error( response ).then( msg => {
+          console.log( msg );
+          process.exit( 1 );
+        });
+        return;
+      }
+      if ( cmd.args[ 1 ] ) {
+        response.pipe(
+          fs.createWriteStream( cmd.args[ 1 ] ).on( 'finish', () =>
+            ! cmd.silent && console.log(
+              `Saved to ${cmd.args[ 1 ]} (${fs.statSync( cmd.args[ 1 ] ).size} B).`
+            )
+          )
+        );
+        return;
+      }
+      response.pipe( concat( buffer => {
+        istextorbinary.isText( undefined, buffer, ( err, is_text ) =>
+          console.log( err || is_text
+            ? buffer.toString()
+            : 'Not going to show a binary file. Run the command to save it as a file.'
+          )
+        );
+      }));
+    });
+  }).catch( e => console.log( e ) );
+
+  return;
+}
 
 function login ( host_url ) {
   return new Promise( ( resolve, reject ) => {
@@ -118,16 +101,16 @@ function login ( host_url ) {
 }
 
 function query_credentials () {
-  console.log( 'Please enter your Sharepoint credentials' );
+  console.log( 'Please enter your Sharepoint credentials'.gray );
   let query = ( read_opts ) =>
     new Promise( ( resolve, reject ) =>
       read( read_opts, ( err, result ) => err ? reject() : resolve( result ) )
     );
   return query({
-    prompt  : 'Email address :'
+    prompt  : 'Email address :'.cyan.bold
   })
   .then( username => query({
-    prompt  : 'Password      :',
+    prompt  : 'Password      :'.cyan.bold,
     silent  : true,
     replace : '*'
   })
@@ -168,50 +151,141 @@ function parse_request_error ( response ) {
   });
 }
 
-let
-  parsed      = process.argv[ 3 ] ? url.parse( process.argv[ 3 ] ) : '',
-  host_url    = `${parsed.protocol}//${parsed.host}`,
-  output_path = process.argv[ 4 ] ? process.argv[ 4 ] : '';
-
-if ( command === 'login' ) {
-  login( host_url ).then( () => console.log( 'Logged in.' ) ).catch(
-    e => console.log( e )
+function get_cookie_path ( opts ) {
+  opts = opts || {};
+  let cookie_dir = path.join(
+    process.env.HOME || process.env.USERPROFILE || process.env.HOMEPATH,
+    '.sharepoint-file'
   );
-  return;
+  ! fs.existsSync( cookie_dir ) && fs.mkdirSync( cookie_dir );
+  return opts.dir ? cookie_dir : path.join( cookie_dir, 'cookies.json' );
 }
 
-if ( command === 'fetch' ) {
-  login( host_url ).then( jar => {
-    output_path.length && console.log( `Fetching file...` );
-    request({
-      url : `${host_url}/_api/web/GetFileByServerRelativeUrl('${parsed.path}')/$value`,
-      jar : jar
-    }).on( 'error', err => console.log( err ) ).on( 'response', response => {
-      if ( response.statusCode !== 200 ) {
-        parse_request_error( response ).then( msg => {
-          console.log( msg );
-          process.exit( 1 );
-        });
-        return;
+function parse ( args ) {
+  let
+    opts = {
+      'string'  : [ 'login', 'fetch' ],
+      'boolean' : [ 'silent', 'help', 'version' ],
+      'default' : {
+        'silent'  : false,
+        'help'    : false,
+        'version' : false
       }
-      if ( output_path.length ) {
-        response.pipe(
-          fs.createWriteStream( output_path ).on( 'finish', () => console.log(
-            `Saved to ${output_path} (${fs.statSync( output_path ).size} B).`
-          ))
-        );
-        return;
-      }
-      response.pipe( concat( buffer => {
-        istextorbinary.isText( undefined, buffer, ( err, is_text ) =>
-          console.log( err || is_text
-            ? buffer.toString()
-            : 'Not going to show a binary file. Run the command to save it as a file.'
-          )
-        );
-      }));
-    });
-  }).catch( e => console.log( e ) );
+    },
+    argv = parseArgs( args, opts );
+  return validate({
+    name    : argv._[ 0 ],
+    args    : argv._.slice( 1 ),
+    silent  : argv.silent,
+    help    : argv.help,
+    version : argv.version
+  });
 
-  return;
+  function validate ( cmd ) {
+    if ( ! cmd.name ) {
+      if ( cmd.help    ) return show_info( cmd.silent );
+      if ( cmd.version ) return show_info( cmd.silent, { version : true } );
+      return show_info( cmd.silent, { minimal : true } );
+    }
+
+    if ( opts[ 'string' ].indexOf( cmd.name ) === -1 ) {
+      return show_info( cmd.silent, { invalid : true, minimal : true } );
+    }
+
+    if ( cmd.help ) {
+      return show_info( cmd.silent, { cmd : cmd.name } );
+    }
+
+    if ( cmd.name === 'login' && ! cmd.args.length ) {
+      return show_info( cmd.silent, { invalid : true, cmd : cmd.name } );
+    }
+
+    if ( cmd.name === 'fetch' && ! cmd.args.length ) {
+      return show_info( cmd.silent, { invalid : true, cmd : cmd.name } );
+    }
+
+    return cmd;
+  }
+}
+
+function extract ( args, name ) {
+  if ( name === 'host_url' ) {
+    let parsed = url.parse( args[ 0 ] );
+    return `${parsed.protocol}//${parsed.host}`;
+  }
+}
+
+function show_info ( silent, opts ) {
+  opts = opts || {};
+  const
+    lines = ( lines ) => lines.map( line => line + os.EOL ).join( '' ),
+    version = `${require( '../package.json' ).name } ver. ${require( '../package.json' ).version.bold }`,
+    header = [
+      `A command-line utility for Sharepoint file operations`,
+      ``,
+      `Usage: spfile task args`,
+      ``,
+      `========================`,
+      ``,
+      `${'Available tasks:'.bold} (use --help for more info)`
+    ],
+    info = {
+      fetch : {
+        minimal : show_args =>
+          `   ${'fetch '.green.bold + (show_args ? '<FILEURL> [FILEPATH] '.green.bold + '...' : ' ....')}`
+            + ` Fetches a file and shows its content or saves it`.bold,
+        full : [
+          `                                  <FILEURL>  The full Sharpoint URL to the file`,
+          `                                  [FILEPATH] File name or file path to save to`,
+          ``,
+          `                                  If you haven't already authenticated or your session has expired,`,
+          `                                  you'll be asked to log in`,
+          ``,
+          `                                  Example: spfile fetch https://your.sharepoint.com/path/foo.json`,
+          `                                  Example: spfile fetch https://your.sharepoint.com/path/bar.pdf bar.pdf`
+        ]
+      },
+      login : {
+        minimal : show_args =>
+          `   ${'login '.green.bold + (show_args ? '<HOSTURL> '.green.bold + '..............' : ' ....')}`
+            + ` Authenticates explicitly with Sharepoint`.bold,
+        full : [
+          `                                  <HOSTURL> The Sharpoint host URL`,
+          ``,
+          `                                  Example: spfile login https://your.sharepoint.com`
+        ]
+      },
+      logout : {
+        minimal : show_args =>
+          `   ${'logout '.green.bold + (show_args ? '.......................' : ' ...')}`
+            + ` Invalidates your Sharepoint session explicitly`.bold,
+        full : [
+        ]
+      }
+    };
+
+  if ( silent ) return;
+  if ( opts.version ) {
+    console.log( version );
+    return;
+  }
+  if ( opts.invalid ) {
+    console.log( `Invalid command or parameters${os.EOL}` );
+  }
+  if ( opts.cmd ) {
+    console.log(
+      lines( [ info[ opts.cmd ].minimal(true) ].concat( info[ opts.cmd ].full ) )
+    );
+    return;
+  }
+
+  console.log( lines( [ version ].concat( header ) ) );
+  Object.keys( info ).map( c => {
+    if ( opts.minimal ) {
+      console.log( lines([ info[ c ].minimal() ]) )
+    }
+    else {
+      console.log( lines([ info[ c ].minimal(true) ].concat( info[ c ].full ) ) )
+    }
+  });
 }
